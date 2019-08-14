b0aaeff4f14fe47b5240c0f36c7a61559e814f49
Corey Moore <monorthwest@gmail.com>
2019-08-14 11:09:12 -0700

    in config.js changed db_url to database_url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgres://coreymoore@localhost/makersplace',
+  DB_URL: process.env.DATABASE_URL || 'postgres://coreymoore@localhost/makersplace',
   JWT_SECRET: process.env.JWT_DECRET || 'this-is-a-secret'
 };
